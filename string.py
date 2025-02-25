12c856409d9761a1038572a14d66c7cb5faa7026
antonorlov666 <moyrebenokorlov@gmail.com>
2025-02-25 19:47:01 +0700

    Delete string.py

--- string.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd #
-
-df = pd.read_excel('КОЕ_Поверхность_кожи_живота_змеи.xlsx') #
-
-string = int(input('Bitte geben Sie eine Zeichenfolge ein: ')) #
-
-print(df.iloc[:,string]) #
